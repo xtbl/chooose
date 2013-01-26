33c5889244e2d99728c4d174288f754ed945fd48
xtbl <correodecristobal@gmail.com>
2013-01-26 01:04:00 -0600

    basic clicks and fades

--- js/app.js
+++ js/app.js
@@ -27,15 +27,18 @@
 //console.log(obj[name]);
 // iterates through objects with each click
 
+// start with facade and then move to a constructor like the one in page 100
 
 var CHOOO = CHOOO || {};
 
 	CHOOO = (function(){
 	// private properties
 	var counter,
+		position = 0,
 		config = {
-		resultContainer: '#result',
-		consoleMsgs: true
+			container: '#container',
+			resultContainer: '#result', // this will be replaced by parameter in constructor
+			consoleMsgs: true
 	},
 
  // check 
@@ -43,20 +46,62 @@ var CHOOO = CHOOO || {};
  // http://stackoverflow.com/questions/180844/referencing-a-javascript-object-literal-array
 		selectionList = [
 			{ 
-				title: 'default title',
-				img_src: 'default_imgsource.jpg',
+				id: '01',
+				title: '01_01',
+				img_src: '01_01.jpg',
 				description: 'default description'
 			},
 			{ 
-				title: 'default title',
-				img_src: 'default_imgsource.jpg',
+				id: '02',
+				title: '01_02',
+				img_src: '01_02.jpg',
 				description: 'default description'
-			},
+			}
 
 		]
 	;
 	// public api
 	return {
+		selectionList: [
+			{ 
+				id: '01',
+				title: '01_01',
+				img_src: '01_01.jpg',
+				description: 'default description'
+			},
+			{ 
+				id: '02',
+				title: '01_02',
+				img_src: '01_02.jpg',
+				description: 'default description'
+			}
+
+		],
+		attachEventListeners: function () {
+			console.log( 'this is container:' + container );
+
+			$(config.container + ' .option').on('click',function(e){
+				console.log($(this));
+				// add displayResult etc here
+			});
+
+			$(config.container + ' #option-A h3').animate({
+			  opacity: 0
+			}, {
+			  duration: 1800,
+			  easing: 'ease-out',
+			  complete: function() { /* ... */ }
+			});
+		},
+		traverseSelection: function () {
+			for( var i = 0; i < selectionList.length; i++){
+				console.log(selectionList[i].title);
+			};
+		},
+		testNextSelection: function () {
+			console.log( selectionList[position].title );
+			position = position + 1;	
+		},
 		displayOptions: function () {
 			// display options a and b with their properties, including right or wrong
 			if(config.consoleMsgs){
